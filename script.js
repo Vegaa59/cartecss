e50534fada826b8dd60731aa0882d04643f27e95
Vegaa59 <60628118+Vegaa59@users.noreply.github.com>
2020-09-08 15:20:01 +0200

    responsive

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 const sr = ScrollReveal();
 
 sr.reveal(".carte", {
-    origin: "right",
-    distance: "400px",
-    delay: 200,
-    duration: 1000,
-    interval: 300,
-  });
-  sr.reveal(".arrow", {
-    origin: "right",
-    distance: "400px",
-    delay: 200,
-    duration: 1000,
-    interval: 300,
-  });
+  origin: "right",
+  distance: "400px",
+  delay: 300,
+  duration: 1000,
+  interval: 500,
+});
+sr.reveal(".arrow", {
+  origin: "right",
+  distance: "400px",
+  delay: 300,
+  duration: 1000,
+  interval: 500,
+});
+sr.reveal(".competence", {
+  origin: "bottom",
+  distance: "400px",
+  delay: 800,
+  duration: 1000,
+  interval: 500,
+});
