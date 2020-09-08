ae3b19c9aed8769db6f6c2fc834724305c5189e4
vegaa59 <sattplak@hotmail.fr>
2020-09-08 13:48:37 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const sr = ScrollReveal();
+
+sr.reveal(".carte", {
+    origin: "right",
+    distance: "400px",
+    delay: 200,
+    duration: 1000,
+    interval: 300,
+  });
+  sr.reveal(".arrow", {
+    origin: "right",
+    distance: "400px",
+    delay: 200,
+    duration: 1000,
+    interval: 300,
+  });
